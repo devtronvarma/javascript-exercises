9dd450264aadf442b36c1118cab3978cc38ca28b
Dev Varma <devtronvarma@gmail.com>
2022-07-27 10:09:22 -0500

    Complete exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let output = [];
+    for (i = words.length - 1; i >= 0; i--) {
+        output.push(words[i]);
+    }
+    return output.join('');
 };
 
 // Do not edit below this line
