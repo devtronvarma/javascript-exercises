0f1884b430a43f6da2783aab805fdb4ab83309fe
Dev Varma <devtronvarma@gmail.com>
2022-07-27 14:51:01 -0500

    Complete exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,26 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+  const result = (degrees - 32) * 5/9
+  
+  if (result % 1 == 0) {
+    return result;
+  }
+  
+  else {
+    return (Math.round(result * 1e1) / 1e1);
+  }
 };
 
-const ctof = function() {
+const ctof = function(degrees) {
+  const result = degrees * 9/5 + 32
+  
+  if (result % 1 == 0) {
+    return result;
+  }
 
+  else {
+    return (Math.round(result * 1e1) / 1e1);
+  }
+  
 };
 
 // Do not edit below this line
