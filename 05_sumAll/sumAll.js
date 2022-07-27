fb763a95d6ce0183ac84660f6da2fbb11bdb8337
Dev Varma <devtronvarma@gmail.com>
2022-07-27 14:10:34 -0500

    Complete exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    result = 0;
 
+    if (typeof(a) != "number" || typeof(b) != "number" || a < 0 || b < 0) {
+        return 'ERROR'
+    }
+
+    else if (a >= b) {
+        for (let i = b; i <= a; i++) {
+            result += i;
+        }
+    }
+
+    else {
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
