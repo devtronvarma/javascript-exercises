0bca05922d80bd5d03008a1ce44feecf518dd6e8
Dev Varma <devtronvarma@gmail.com>
2022-07-26 22:00:47 -0500

    Complete exercise 02: repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num >= 0) {
+        
+        return word.repeat(num);
 
+        }
+
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
